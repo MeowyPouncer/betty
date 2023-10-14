b7f3f612ecc48666b0191c01602d119d938eb855
Weekrow <somewherelse@yandex.ru>
2023-10-14 11:45:26 +0300

    Ceremonies html desktop version upload

--- js/main.js
+++ js/main.js
@@ -7,11 +7,11 @@ document.addEventListener('DOMContentLoaded', function () {
             const buttonIcon = this.querySelector('.ceremonies-list__button img');
             if (text.style.maxHeight) {
                 text.style.maxHeight = null;
-                buttonIcon.src = '/images/faq-button-plus.svg';
+                buttonIcon.src = 'images/faq-button-plus.svg';
 
             } else {
                 text.style.maxHeight = text.scrollHeight + 'px';
-                buttonIcon.src = '/images/faq-button.svg';
+                buttonIcon.src = 'images/faq-button.svg';
             }
         });
     });
